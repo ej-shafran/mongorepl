3257d2d254e9d5890fa5e30a40b75953bc708709
ej-shafran <ejshafran@hilma.tech>
2023-08-29 01:43:49 +0300

    feat: add configuration options for `tee` mode

--- scripts/tee.js
+++ scripts/tee.js
@@ -9,14 +9,15 @@ const __dirname = path.dirname(__filename);
 const index = path.join(__dirname, "../src/index.js");
 const env = path.join(__dirname, "../.env");
 const bin = path.join(__dirname, "../bin/main.js");
-const out = path.join(__dirname, "../out");
+const out =
+  process.env.TEE_OUT || process.argv[2] || path.join(__dirname, "../out");
 
 nodemon({
   ext: "*",
   env: {
-    DISABLE_COLORS: "y",
-    LOG_PROMPT: "y",
-    DISABLE_WARNINGS: "y"
+    DISABLE_COLORS: process.env.DISABLE_COLORS ?? "y",
+    LOG_PROMPT: process.env.LOG_PROMPT ?? "y",
+    DISABLE_WARNINGS: process.env.DISABLE_WARNINGS ?? "y",
   },
   watch: [index, env],
   exec: `node ${bin} | tee ${out}`,
